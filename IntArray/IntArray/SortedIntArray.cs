d5dcb1450119f8d832750402975e1a29d036eefc
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-08-22 20:22:00 +0300

    new SortIntArray

--- IntArray/IntArray/SortedIntArray.cs
+++ IntArray/IntArray/SortedIntArray.cs
@@ -6,20 +6,17 @@ namespace IntArrays
 {
     class SortedIntArray : IntArray
     {
-        public SortedIntArray() : base()
+        public SortedIntArray(int[] array)
         {
-            void Sort(int[] array)
+            for (int i = 0; i < array.Length - 1; i++)
             {
-                for (int i = 0; i < array.Length - 1; i++)
+                for (int j = i + 1; j < array.Length; j++)
                 {
-                    for (int j = i + 1; j < array.Length; j++)
+                    if (array[j] < array[i])
                     {
-                        if (array[j] < array[i])
-                        {
-                            int temp = array[i];
-                            array[i] = array[j];
-                            array[j] = temp;
-                        }
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
                     }
                 }
             }
