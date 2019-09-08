0fad15d07b1c42ceca074fd757f77d4444c885fd
Bogdan512 <ciurtinbogdan@yahoo.com>
2019-09-08 12:30:31 +0300

    Insert final

--- IntArray/IntArray/SortedIntArray.cs
+++ IntArray/IntArray/SortedIntArray.cs
@@ -35,7 +35,7 @@ namespace IntArrays
         public new void Insert(int index, int element)
             {
             int getValueOrDefault = GetValueOrDefault(index - 1, index);
-            if ((element < getValueOrDefault || element > this[index]) && (element < getValueOrDefault || Count != index))
+            if (GetValueOrDefault(index - 1, element) > element || element > GetValueOrDefault(index, element))
             {
                 return;
             }
